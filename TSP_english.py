33a6ff3de61937396d22d2bf0ed7888a558cce75
Janik <janik@klauenberg.eu>
2020-06-14 14:05:35 +0200

    changed TSP_english.py to english function names

--- TSP_english.py
+++ TSP_english.py
@@ -14,104 +14,104 @@ if min_py_ver>py_ver:
     sys.exit()
 
 class SpeditionsProblem():
-    def __init__(self, dists, startpunkt):
+    def __init__(self, dists, startingpoint):
         """
-        Kommentar: Klasse die zur loesung des Traveling Salesmen Problem gedacht ist
-        Input: Name der Instanz, Distanzen in form eines Dictionarys und ein Startpunkt welcher im Dictionary enthalten sein MUSS
-        Output: Keins, da Init
-        Besonders: Ruft methode get_alle_pfade auf, Es muss von jedem Punkt eine angabe zur Distanz zu jedem anderen Punkt geben
+        Comment: Class for solving the Traveling Salesmen Problem
+        Input: Name of instance, Distanz as a Dictionary and a startingpoint which must be in the Dictionary
+        Output: Nothing, just a standard Init
+        Special: Uses method get_alle_pathe, there HAS TO BE a connection from every node to every other node
         """
-        self.distanzen = dists
-        self.start = startpunkt
-        self.geloest = False
-        self.get_alle_pfade()
+        self.distances = dists
+        self.start = startingpoint
+        self.solved = False
+        self.get_all_paths()
 
-    def get_alle_pfade(self):
+    def get_all_paths(self):
         """
-        Kommentar: Erzeugt alle moeglichen Pfade fuer die Instanz
+        Kommentar: Erzeugt alle moeglichen pathe fuer die Instanz
         Input: Name der Instanz
         Output: Keins, erzeugt oder aendert lokale Variable alle_moeglichen
         Besonders: Keine Besonderheiten
         """
-        main_pfad = []
-        for i in self.distanzen:
-            main_pfad.append(i)
-        main_pfad.remove(self.start)
-        alle = list(itertools.permutations(main_pfad))
-        for j in range(len(alle)):
-            alle[j] = [list(alle[j])]
-            alle[j][0].insert(0, self.start)
-            alle[j][0].append(self.start)
-        self.alle_moeglichen = alle
-    def get_distanz(self, pfad):
+        main_path = []
+        for i in self.distances:
+            main_path.append(i)
+        main_path.remove(self.start)
+        all = list(itertools.permutations(main_path))
+        for j in range(len(all)):
+            all[j] = [list(all[j])]
+            all[j][0].insert(0, self.start)
+            all[j][0].append(self.start)
+        self.all_possible = all
+    def get_distances(self, path):
         """
-        Kommentar: Sucht fuer einen Pfad die gesamtdistanz und gibt diese mittels return konstruktor zurueck
-        Input: Name der Instanz, Pfad von welchem die Distanz benoetigt wird
-        Output: Distanz eines Pfades mittels Return konstruktor
+        Kommentar: Sucht fuer einen path die gesamtdistanz und gibt diese mittels return konstruktor zurueck
+        Input: Name der Instanz, path von welchem die Distanz benoetigt wird
+        Output: Distanz eines pathes mittels Return konstruktor
         Besonders: Es muessen Verbindungen von ALLEN knoten untereinander existieren
         """
         res = 0
-        for i in range(len(pfad)-1):
+        for i in range(len(path)-1):
             try:
-                res += self.distanzen[pfad[i]][pfad[i+1]]
+                res += self.distances[path[i]][path[i+1]]
             except KeyError:
                 res += hash(float("inf"))
         return res
-    def get_all_distanzen(self):
+    def get_all_distances(self):
         """
         Kommentar: Nutzt get_distanz() um die distanz von allen moeglichen Wegen zu ermitteln
         Input: Name der Instanz
         Output: Kein direktes, aendert lokale variable alle_moeglichen
         Besonders: Kein direktes Return oder so, prueft ob problem bereits geloest wurde
         """
-        if self.geloest:
+        if self.solved:
             return
-        temp = self.alle_moeglichen
+        temp = self.all_possible
         for i in range(len(temp)):
-            dist = self.get_distanz(temp[i][0])
+            dist = self.get_distances(temp[i][0])
             temp[i].insert(0, dist)
         temp.sort()
-        self.alle_moeglichen = temp
+        self.all_possible = temp
     def solve_problem(self):
         """
-        Kommentar: loest das Problem indem es die Methode get_all_distanzen aufruft
+        Kommentar: loest das Problem indem es die Methode get_all_distances aufruft
         Input: Name der Instanz
         Output: Kein direktes, lokale variable alle_moeglichen wird veraendert
         Besonders: prueft ob problem bereits geloest wurde
         """
-        if not self.geloest:
-            self.get_all_distanzen()
-            self.geloest = True
+        if not self.solved:
+            self.get_all_distances()
+            self.solved = True
         else:
             pass
-    def get_loesung(self):
+    def get_solution(self):
         """
-        Kommentar: Fuehrt Methode get_all_distanzen aus und gibt schnellsten weg aus
+        Kommentar: Fuehrt Methode get_all_distances aus und gibt schnellsten weg aus
         Input: Name der Instanz
         Output: Kein direktes
         Besonders: prueft ob problem bereits geloest wurde
         """
         self.solve_problem()
-        return self.alle_moeglichen[0]
+        return self.all_possible[0]
     def export_loesung(self, filename=None):
         """
-        Kommentar: Exportiert die Loesung sowie alle anderen moeglichen Pfade mit gewicht
+        Kommentar: Exportiert die Loesung sowie alle anderen moeglichen pathe mit gewicht
         Input: name der Instanz, optional: Name der Datei
         Output: Kein direktes Output
         Besonders: Optimiert fuer python 3 und python 3
         """
         res = []
-        if not self.geloest:
+        if not self.solved:
             self.solve_problem
         if not filename:
             filename = str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + "--SpeditionsProblem")
-        for i in range(len(self.alle_moeglichen)):
-            res.append([self.alle_moeglichen[i][0]])
-            for k in self.alle_moeglichen[i][1]:
+        for i in range(len(self.all_possible)):
+            res.append([self.all_possible[i][0]])
+            for k in self.all_possible[i][1]:
                 res[i].append(k)
         filename = filename + ".csv"
         filename = os.path.join("csv", filename)
-        first_row = ["Distanz", "Pfad"]
+        first_row = ["Distanz", "path"]
         print (res)
         global x
         if x[0]==3:
@@ -143,7 +143,7 @@ if __name__ == '__main__':
 
     erster = SpeditionsProblem(distanzen, 0)
     print ("Bester Weg:")
-    print (erster.get_loesung())
+    print (erster.get_solution())
     print ("\nAlle Wege:")
-    print (erster.alle_moeglichen)
-    print (erster.export_loesung())
+    print (erster.all_possible)
+    #print (erster.export_loesung())
